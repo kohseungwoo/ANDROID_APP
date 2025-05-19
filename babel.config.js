0b1924fbfd97d775f80eecf8519f2e9e1a1396f4
swkoh <swkoh>
2025-05-19 16:00:05 +0900

    fix : ios(test)

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  presets: [
-    ['module:@react-native/babel-preset', {
-      useTransformReactJSXExperimental: true,
-    }],
-  ],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     'react-native-reanimated/plugin', // 꼭 맨 아래에 위치해야 합니다!
   ],
--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -3,26 +3,20 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 
-import login from '../pages/login/SignInScreen';
-import paymentMenu from '../pages/payment/PaymentMenuScreen';
-import trxDetail from '../pages/trxboard/TrxDetailScreen';
-import TabNavigator from '../components/TabNavigator';
-import usePortraitLock from './hooks/UnlockHooks';
-import noticeScreen from '../pages/notice/NoticeScreen';
+import TestScreen from '../pages/TestScreen'; // ✅ 경로 확인
+import {Alert} from 'react-native';
 
 const Stack = createStackNavigator();
 
 const AppContent = () => {
-    usePortraitLock();
-
+    Alert.alert("tt");
+    console.log('TestScreen:', TestScreen);
+    // usePortraitLock();
+    //
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="LOGIN" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="LOGIN" component={login} />
-                <Stack.Screen name="DASHBOARD" component={TabNavigator} />
-                <Stack.Screen name="PAYMENTMENU" component={paymentMenu} />
-                <Stack.Screen name="TRXDETAIL" component={trxDetail} />
-                <Stack.Screen name="NOTICE" component={noticeScreen} />
+            <Stack.Navigator initialRouteName="TEST" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="TEST" component={TestScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
