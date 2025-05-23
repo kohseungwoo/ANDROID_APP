fcb76192a4ae68e4cfa5cdf7b4a35be42d8b14cf
swkoh <swkoh>
2025-05-23 11:29:41 +0900

    fix : andorid (appIcon + label, backgroundIcon)

--- android/app/src/main/java/com/tb_android_app/MainActivity.kt
+++ android/app/src/main/java/com/tb_android_app/MainActivity.kt
@@ -9,6 +9,8 @@ import android.os.Bundle
 import com.zoontek.rnbootsplash.RNBootSplash;
 import android.graphics.Color
 import android.view.View
+import android.app.ActivityManager
+import android.graphics.BitmapFactory
 
 class MainActivity : ReactActivity() {
 
@@ -17,7 +19,18 @@ class MainActivity : ReactActivity() {
     override  fun onCreate(savedInstanceState: Bundle?) {
         RNBootSplash.init(this, R.style.BootSplashTheme)
         super.onCreate(savedInstanceState)
+
+        // 최근 앱 썸네일에 로고 표시
+        val icon = BitmapFactory.decodeResource(resources, R.drawable.bootsplash_logo)
+        setTaskDescription(
+            ActivityManager.TaskDescription(
+                "TB_ANDROID_APP",  // 앱 이름
+                icon,
+                Color.WHITE        // 썸네일 배경색
+            )
+        )
     }
+
 
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
