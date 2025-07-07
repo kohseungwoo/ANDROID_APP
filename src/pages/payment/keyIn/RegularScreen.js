56f05ffc73dcf14f2bfa838b6be52655fa902d14
swkoh <swkoh>
2025-07-07 10:28:46 +0900

    fix : 운영반영

--- src/pages/payment/keyIn/RegularScreen.js
+++ src/pages/payment/keyIn/RegularScreen.js
@@ -139,13 +139,13 @@ const RegularScreen = ({ formData, setFormData }) => {
     };
 
     const paymentBtn = async () => {
-        // formData.personalCardNumber1 = '9490';
-        // formData.personalCardNumber2 = '9402';
-        // formData.personalCardNumber3 = '1292';
-        // formData.personalCardNumber4 = '9009';
-        // formData.personalExpiry = '2905';
-        // formData.personalPassword = '00';
-        // formData.dob = '950101';
+        // formData.personalCardNumber1 = '';
+        // formData.personalCardNumber2 = '';
+        // formData.personalCardNumber3 = '';
+        // formData.personalCardNumber4 = '';
+        // formData.personalExpiry = '';
+        // formData.personalPassword = '';
+        // formData.dob = '';
 
         const { cardType, personalCardNumber1, personalCardNumber2, personalCardNumber3, personalCardNumber4, personalInstallment ,personalExpiry ,personalPassword ,dob
             ,corpCardNumber1 ,corpCardNumber2 ,corpCardNumber3 ,corpCardNumber4 ,corpInstallment ,corpExpiry ,corpPassword ,brn } = formData;
